375b5ff6e693e45fec551356468e1a899ce5c4aa
sfc-gh-chguzman <christopher.guzman@snowflake.com>
2025-10-15 12:35:18 -0500

    Adding files to git ignore

--- bc_dr/600_enable_failover_sa.sql
+++ bc_dr/600_enable_failover_sa.sql
@@ -9,3 +9,41 @@ create failover group sales_payroll_failover
     allowed_shares = global_sales_share
     allowed_accounts = <org_name.account_name>
 --  replication_schedule = '180 MINUTES';
+
+
+ALTER FAILOVER GROUP sales_payroll_failover SET allowed_databases = global_sales,common,payroll,inventory,loyalty,COMMON_UTILITY,DEMO_DB,SECURITY_NETWORK_DB;
+ALTER FAILOVER GROUP sales_payroll_failover SET object_types = users, roles, warehouses, resource monitors, databases, shares, account parameters, NETWORK POLICIES
+;
+ALTER FAILOVER GROUP sales_payroll_failover SET allowed_accounts = SFPSCOGS.CHGUZMAN_AZURE_DEMO;
+
+SHOW FAILOVER GROUPS;
+
+ALTER FAILOVER GROUP ADD 
+    NETWORK POLICY 'ACCOUNT_VPN_POLICY_SE';
+
+  SELECT * 
+    FROM TABLE(
+      INFORMATION_SCHEMA.REPLICATION_GROUP_DANGLING_REFERENCES('sales_payroll_failover')
+  );
+
+SHOW FAILOVER GROUPS;
+SHOW DATABASES IN FAILOVER GROUP sales_payroll_failover;
+SHOW SHARES IN FAILOVER GROUP sales_payroll_failover;
+
+SHOW REPLICATION ACCOUNTS ->> SELECT * FROM $1 ;
+
+SHOW USERS;
+DROP USER CHGUZMAN_1;
+
+ALTER USER CHGUZMAN UNSET SESSION POLICY;
+
+ALTER FAILOVER GROUP sales_payroll_failover
+  ADD POLICY SECURITY_NETWORK_DB.POLICIES.ACCOUNT_MFA_AUTH_POLICY;
+
+
+DESCRIBE NETWORK POLICY SECURITY_NETWORK_DB.POLICIES.ACCOUNT_MFA_AUTH_POLICY;
+SHOW NETWORK POLICIES;
+
+SHOW PARAMETERS IN USER CHGUZMAN ->> SELECT * FROM $1 WHERE "key" LIKE '%NETWORK%';
+
+SHOW failover groups;
